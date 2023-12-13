75e173047c22de3deeb3202312d16da6a7a32c51
Lovro Markovic <lovro.markovic@fer.hr>
2023-12-13 16:41:13 +0100

    Update firmware name

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.4.3"
+#define THISFIRMWARE "Copter-Larics-v4.4.3 926a0dc"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,4,3,FIRMWARE_VERSION_TYPE_OFFICIAL
