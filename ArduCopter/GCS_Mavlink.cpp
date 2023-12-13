07fb299c4576f5a501c684db8b6ce61a3a628d85
Lovro Markovic <lovro.markovic@fer.hr>
2023-12-13 15:59:34 +0100

    Add yawrate from 90ad7b5

--- ArduCopter/GCS_Mavlink.cpp
+++ ArduCopter/GCS_Mavlink.cpp
@@ -1188,8 +1188,9 @@ void GCS_MAVLINK_Copter::handleMessage(const mavlink_message_t &msg)
             ang_vel.z = packet.body_yaw_rate;
         }
 
+        bool use_yawrate = !yaw_rate_ignore && pitch_rate_ignore && roll_rate_ignore;
         copter.mode_guided.set_angle(attitude_quat, ang_vel,
-                climb_rate_or_thrust, use_thrust);
+                climb_rate_or_thrust, use_thrust, use_yawrate);
 
         break;
     }
